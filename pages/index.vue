9b75d3f3ca6ce4c9df5d617a058f2cf1d123351f
SammieKei40 <sammiekei9@gmail.com>
2023-01-23 10:55:34 +0100

    Third commit

--- pages/index.vue
+++ pages/index.vue
@@ -45,7 +45,7 @@
 
 <ol class="relative ml-5 mt-5 text-gray-500 border-l-2 border-gray-200 " 
 >                  
-    <li class="mb-10 ml-6" @click="changeStep(1)" :class="[ ifActive ? 'border-blue-200 text-black' :  'border-gray-200 text-gray-200' ]">
+    <li class="mb-10 ml-6" @click="changeStep(1)" :class="[ step === 1 ? 'border-blue-200 text-black' :  'border-gray-200 text-gray-200' ]">
       <span class="flex flex-row gap-5 cursor-pointer" >
         <!-- <span class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900"> -->
             <svg aria-hidden="true"  class=" bg-blue-500 text-white w-8 h-8 rounded-full" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
@@ -61,7 +61,7 @@
       </span>
         </span>            
     </li>
-    <li class="mb-10 ml-6 cursor-pointer" @click="changeStep(2)" :class="[ ifActive ? 'border-blue-200 text-black' :  'border-gray-200 text-gray-200' ]">
+    <li class="mb-10 ml-6 cursor-pointer" @click="changeStep(2)" :class="[ step === 2 ? 'border-blue-200 text-black' :  'border-gray-200 text-gray-200' ]">
       <span class="flex flex-row gap-5" >
         <!-- <span class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900"> -->
             <svg aria-hidden="true" class=" bg-blue-200 text-blue-500 w-8 h-8 rounded-full" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
@@ -77,7 +77,7 @@
       </span>
         </span>            
     </li>
-    <li class="mb-10 ml-6 cursor-pointer" @click="changeStep(3)" :class="[ ifActive ? 'border-blue-200 text-black' :  'border-gray-200 text-gray-200' ]">
+    <li class="mb-10 ml-6 cursor-pointer" @click="changeStep(3)" :class="[ step === 3 ? 'border-blue-200 text-black' :  'border-gray-200 text-gray-200' ]">
       <span class="flex flex-row gap-5">
         <!-- <span class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900"> -->
             <svg aria-hidden="true" class=" bg-blue-200 text-blue-500 w-8 h-8 rounded-full" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
@@ -102,7 +102,7 @@
 import {ref} from "vue"
 
 const step = ref(1)
-const ifActive = ref(false)
+
 
 function changeStep(stepVal){
   step.value = stepVal
